374842a9c56660f1e01fb2860dcd61554be3506c
Leon Reed <leapcoda@gmail.com>
2026-06-06 21:25:16 +0800

    feat(stores): 初始化状态管理

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import SvgIcon from '@/components/SvgIcon.vue';
 import router from './router';
+import pinia from './stores';
 
 import '@/styles/main.scss';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 app.component('svg-icon', SvgIcon);
 
 app.mount('#app');
