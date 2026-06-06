29471a969b1ea6245bdec618a67fa00657733208
Leon Reed <leapcoda@gmail.com>
2026-06-06 18:35:35 +0800

    feat(router): 初始化路由配置

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import SvgIcon from '@/components/SvgIcon.vue';
+import router from './router';
 
 import '@/styles/main.scss';
 
 const app = createApp(App);
 
+app.use(router);
 app.component('svg-icon', SvgIcon);
 
 app.mount('#app');
